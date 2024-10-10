739224eb662e67b6c9c04cfc048e83ec81614336
anaturko <ana.turko.ferreira@escola.pr.gov.br>
2024-10-10 07:46:19 -0300

    gg

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Astronomia',
+    '1)Qual é o maior planeta do nosso sistema solar?',
+    'Júpiter.')
+
+criaCartao(
+    'Astronomia',
+    '2)Qual é o maior satélite natural do nosso sistema solar?',
+    'Ganimedes, que orbita Júpiter.')
+
+criaCartao(
+    'Astronomia',
+    'O que é um eclipse solar?',
+    'Um eclipse solar ocorre quando a Lua fica entre a Terra e o Sol, bloqueando a luz solar e lançando uma sombra na Terra.')
+
+criaCartao(
+    'Astronomia',
+    '4) Qual foi o nome da primeira missão tripulada a pousar na Lua?',
+    'Apollo 11')
+
+criaCartao(
+    'Matemática',
+    '5)Qual é a fórmula para calcular a área de um círculo?',
+    'Área = π * r²')
+
+criaCartao(
+    'Matemática',
+    '6)Qual é o valor de 7^2 - 4^2?',
+    '45 (49 - 16 = 33).')
+
+criaCartao(
+    'Matemática',
+    '7) Qual é o valor de log 1000?',
+    '3')
+
+criaCartao(
+    'Matemática',
+    '8)Qual é o valor do sin de 45°?',
+    '√2/2')
+
+criaCartao(
+    'Matemática',
+    '9)Qual é a fórmula para o cálculo da probabilidade de eventos independentes?',
+    'P( A e B ) = P (A) x P(B)')
+
+criaCartao(
+    'História',
+    '10) Qual foi o principal objetivo da Revolução Francesa de 1789?',
+    'Estabelecer uma república e acabar com a monarquia absolutista')
+
+criaCartao(
+    'História',
+    '11) Qual foi o nome do tratado que encerrou a Primeira Guerra Mundial?',
+    'Tratado de Versalhes')
+
+criaCartao(
+    'História',
+    '12)Qual era o nome do sistema político e econômico predominante na União Soviética sob Lenin e Stalin?',
+    'Comunismo')
+
+criaCartao(
+    'História',
+    '13)Em que ano o Brasil declarou sua independência de Portugal?',
+    '1822')
+
+criaCartao(
+    'História',
+    '14) Quem foi o líder do movimento de reforma agrária conhecido como "Cangaço", que atuou principalmente no sertão nordestino?',
+    'Lampião (Virgulino Ferreira da Silva)')
+
+criaCartao(
+    'História',
+    '15)Qual foi a grande epidemia que devastou a Europa no século XIV?',
+    'Peste Negra (ou Peste Bubônica)')
+
+criaCartao(
+    'Geografia',
+    '16) Qual é o maior deserto do mundo em área?',
+    'Deserto da Antártida')
+
+criaCartao(
+    'Geografia',
+    '17)Qual é o rio que atravessa a cidade de Paris?',
+    'Rio Sena')
+
+criaCartao(
+    'geografia',
+    '18)Qual é o menor país do mundo em termos de área?',
+    'Cidade do Vaticano')
+
+criaCartao(
+    'Conhecimento Geral',
+    '19)Qual é a unidade de medida usada para quantificar a intensidade de um terremoto?',
+    'Escala Richter')
+
+criaCartao(
+    'Conhecimento Geral',
+    '20)Qual é o país de origem da empresa de tecnologia Samsung?',
+    'Coreia do Sul')
